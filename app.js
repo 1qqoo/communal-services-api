e31bde58ebeaf8e7a28ad5dc3950e3a33f3043fe
Roman Musienko <musienko.roma@mail.ru>
2024-04-15 12:10:27 +0300

    mongo

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { DEV_DB_ADDRESS } = require('./utils/config');
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect(DEV_DB_ADDRESS);
 
 const app = express();
 
---
+++ utils/config.js
@@ -0,0 +1,5 @@
+const DEV_DB_ADDRESS = 'mongodb://localhost:27017/comunaldb';
+
+module.exports = {
+    DEV_DB_ADDRESS
+}
